cda8d5f07997823fdc3273119159796f1e17955d
Yaseen <yas484@hotmail.com>
2024-02-09 03:57:55 +0000

    creates init method and imports pandas and sqlalchemy

--- db_utils.py
+++ db_utils.py
@@ -1,8 +1,18 @@
 import yaml
+from sqlalchemy import create_engine
+import pandas as pd
 
 
 class RDSDatabaseConnector:
+    def __init__(self,credentials_dict):
+        self.credentials_dict = credentials_dict
+
+    def RDSDatabaseConnector(self):    
+
+        pass
     pass
+
+
 
 def load_credentials(filename):
     with open(filename,'r') as cred_file:
