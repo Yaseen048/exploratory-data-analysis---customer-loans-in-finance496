66a3aba53f78bb9e4262b2e7889d007a57e6c037
Yaseen <yas484@hotmail.com>
2024-02-09 03:26:29 +0000

    creates function that loads yaml and returns dictionary

--- db_utils.py
+++ db_utils.py
@@ -1,5 +1,12 @@
+import yaml
 
 
 class RDSDatabaseConnector:
     pass
 
+def load_credentials(filename):
+    with open(filename,'r') as cred_file:
+        credentials_dict = yaml.safe_load(cred_file)
+    print(credentials_dict)
+
+    return credentials_dict
