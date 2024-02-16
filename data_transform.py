93bce807eada410aaa65fdd5d7c01927f2e91ae0
Yaseen <yas484@hotmail.com>
2024-02-16 04:15:07 +0000

    corrected convert to datetime method

--- data_transform.py
+++ data_transform.py
@@ -12,23 +12,26 @@ class DataTranfrom:
         self.df.info()
 
     def convert_to_datetime(self, column):
-        data_format = '%m-%Y'
-        #self.df[column] = pd.to_datetime(self.df[column], format=data_format)
-
-        self.df[column] = pd.to_datetime(self.df[column], errors='coerce')
-
-        #self.df[column] = self.df[column].apply(parse)
-        #self.df[column] = pd.to_datetime(self.df[column], infer_datetime_format=True, errors='coerce')
-        
+        self.df[column] = self.df[column].astype('datetime64[ns]')
         self.df.info()
         print(self.df[column].head(5))
+
+    def remove_units(self, column):
+        self.df[column] = self.df[column].str.replace(r'\D', '',regex=True)
+        self.df[column] = self.df[column].astype('float')
+        
 
         
 
 
 
 formatted_data = DataTranfrom('loan_payments.csv')
-str_columns = ['grade', 'sub_grade', 'home_ownership', 'verification_status', 'loan_status', 'payment_plan', 'purpose', 'application_type']
-date_columns = ['issue_date', 'earliest_credit_line','last_payment_date', 'last_credit_pull_date']#'next_payment_date'
-formatted_data.convert_to_category(str_columns)
+formatted_data.remove_units('term')
+cat_columns = ['grade', 'sub_grade', 'home_ownership', 'verification_status', 'loan_status', 'payment_plan', 'purpose', 'application_type']
+date_columns = ['issue_date', 'earliest_credit_line','last_payment_date', 'last_credit_pull_date', 'next_payment_date']
+formatted_data.convert_to_category(cat_columns)
 formatted_data.convert_to_datetime(date_columns)
+
+
+
+
