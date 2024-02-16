e2afacedd4cb3d93e9cbd6f2a15e09cf9f7cf577
Yaseen <yas484@hotmail.com>
2024-02-16 03:11:26 +0000

    adds drop row method

--- missing_data.py
+++ missing_data.py
@@ -13,6 +13,9 @@ class DataFrameTransform:
     def drop_col(self, columns):
         self.df.drop(columns, axis=1, inplace=True)
 
+    def drop_row(self, columns):
+        self.df.dropna(subset= columns, inplace=True)
+
     def impute_value(self, column, value):
         self.df[column].fillna(value)
 
@@ -26,5 +29,7 @@ data = DataFrameTransform('loan_payments.csv')
 data.null_count()
 columns = ['mths_since_last_delinq','mths_since_last_record', 'next_payment_date', 'mths_since_last_major_derog']
 data.drop_col(columns)
+data.null_count()
+data.drop_row(['last_payment_date', 'last_credit_pull_date','collections_12_mths_ex_med'])
 data.null_count()
 #data.impute_value('')
